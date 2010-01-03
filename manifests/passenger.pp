e40a9032d1ad493c862323af90c328492ba30511
simpsonjulian <simpsonjulian@gmail.com>
2010-01-03 21:43:56 +0000

    MAke it compatible with new rubygems module

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -9,32 +9,26 @@ class apache::passenger {
     
   }
   exec {
-		"brightbox deb key":
-		  command => "/usr/bin/wget -O /root/brightbox.asc \
-		  http://apt.brightbox.net/release.asc;/usr/bin/apt-key add /root/brightbox.asc",
-		  unless => "/usr/bin/apt-key list | grep -i brightbox";
-	
-	}
+    "brightbox deb key":
+     command => "/usr/bin/wget -O /root/brightbox.asc \
+     http://apt.brightbox.net/release.asc;/usr/bin/apt-key add /root/brightbox.asc",
+     unless => "/usr/bin/apt-key list | grep -i brightbox";
+  
+  }
   
   exec { 
-		"apt get update":
-		  command => "/usr/bin/apt-get update",
-		  require => File["brightbox apt source"],
-  	  subscribe => File["brightbox apt source"],
-  	  refreshonly => true;
-   	}
+    "apt get update":
+      command => "/usr/bin/apt-get update",
+      require => File["brightbox apt source"],
+      subscribe => File["brightbox apt source"],
+      refreshonly => true;
+     }
   
   package { 
-		'libapache2-mod-passenger':
-		  ensure => installed,
-		  require => Exec["apt get update"];
-    'rubygems': 
-      ensure => '1.3.1-2ubuntu1~bbox1',
-  		require => Exec["apt get update"];
-    'ruby1.8-dev': ensure => present;
-    'build-essential': ensure => present;
+     'libapache2-mod-passenger':
+       ensure => installed,
+       require => Exec["apt get update"];
     'fastthread': ensure => present, provider => gem;
     'rails': ensure => '2.3.4', provider => gem;
   }
 }
-node socks { include apache::passenger }
